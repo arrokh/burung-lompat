4af6711068f0f991d57dc6a5649147aefa40529a
Noor Octavian Anwar <alv1n@live.com>
2019-11-22 12:01:58 +0700

    Create and add BirController script to Bird object. Initialize basic functionality

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float jumpForce = 300.0f;
+
+    private bool _isDead = false;
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!_isDead)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Jump");
+                _rigidbody.velocity = Vector2.zero;
+                _rigidbody.AddForce(Vector2.up * jumpForce);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isDead = true;
+    }
+}
