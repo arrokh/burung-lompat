a998989945ba499d53b3c843a459aa08adfa17b9
Noor Octavian Anwar <alv1n@live.com>
2019-11-22 17:20:55 +0700

    Add Obstacle

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
     public GameObject gameOverLabel;
+    public GameObject scoreLabel;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+
+    public int score = 0;
+
 
     void Awake()
     {
@@ -22,6 +27,15 @@ public class GameController : MonoBehaviour
     {
         if (gameOver == true && Input.GetMouseButtonDown(0))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void AddScore()
+    {
+        if (gameOver == true)
+            return;
+
+        score++;
+        scoreLabel.GetComponent<Text>().text = "SCORE : " + score.ToString();
     }
 
     public void BirdDead()
